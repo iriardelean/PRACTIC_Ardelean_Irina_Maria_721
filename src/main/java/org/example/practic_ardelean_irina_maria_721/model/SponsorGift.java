7b46126937cc9d5b772655f8fe65f83b0c74d647
iriardelean <iriardelean33@gmail.com>
2026-01-13 08:24:42 +0200

    json + 2 classes added

--- src/main/java/org/example/practic_ardelean_irina_maria_721/model/SponsorGift.java
+++ src/main/java/org/example/practic_ardelean_irina_maria_721/model/SponsorGift.java
@@ -1,4 +1,80 @@
 package org.example.practic_ardelean_irina_maria_721.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SponsorGift {
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("tributId")
+    private int tributId;
+
+    @JsonProperty("itemName")
+    private String itemName;
+
+    @JsonProperty("value")
+    private int value;
+
+    @JsonProperty("day")
+    private int day;
+
+    public SponsorGift(int id, int tributId, String itemName, int value, int day) {
+        this.id = id;
+        this.tributId = tributId;
+        this.itemName = itemName;
+        this.value = value;
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return "SponsorGift{" +
+                "id=" + id +
+                ", tributId=" + tributId +
+                ", itemName='" + itemName + '\'' +
+                ", value=" + value +
+                ", day=" + day +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTributId() {
+        return tributId;
+    }
+
+    public void setTributId(int tributId) {
+        this.tributId = tributId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
 }
--- src/main/java/org/example/practic_ardelean_irina_maria_721/model/Tribute.java
+++ src/main/java/org/example/practic_ardelean_irina_maria_721/model/Tribute.java
@@ -1,4 +1,79 @@
 package org.example.practic_ardelean_irina_maria_721.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Tribute {
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("distrikt")
+    private int distrikt;
+
+    @JsonProperty("status")
+    private Status status;
+
+    @JsonProperty("skillLevel")
+    private int skillLevel;
+
+    public Tribute(int id, String name, int distrikt, Status status, int skillLevel) {
+        this.id = id;
+        this.name = name;
+        this.distrikt = distrikt;
+        this.status = status;
+        this.skillLevel = skillLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Tribute{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", distrikt=" + distrikt +
+                ", status=" + status +
+                ", skillLevel=" + skillLevel +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDistrikt() {
+        return distrikt;
+    }
+
+    public void setDistrikt(int distrikt) {
+        this.distrikt = distrikt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(int skillLevel) {
+        this.skillLevel = skillLevel;
+    }
 }
